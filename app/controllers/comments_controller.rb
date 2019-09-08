2a5d19b13088ee655debdad0dbb0d9fd219a6af2
Raghu Betina <raghu@firstdraft.com>
2019-09-07 22:32:23 -0500

    Return comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,41 +1,108 @@
 class CommentsController < ApplicationController
+  before_action :current_user_must_be_comment_commenter, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_comment_commenter
+    comment = Comment.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == comment.commenter
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
-    comments = CommentResource.all(params)
-    respond_with(comments)
+    @q = current_user.comments.ransack(params[:q])
+    @comments = @q.result(:distinct => true).includes(:commenter, :photo).page(params[:page]).per(10)
+
+    render("comment_templates/index.html.erb")
   end
 
   def show
-    comment = CommentResource.find(params)
-    respond_with(comment)
+    @comment = Comment.find(params.fetch("id_to_display"))
+
+    render("comment_templates/show.html.erb")
   end
 
-  def create
-    comment = CommentResource.build(params)
+  def new_form
+    @comment = Comment.new
 
-    if comment.save
-      render jsonapi: comment, status: 201
+    render("comment_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @comment = Comment.new
+
+    @comment.body = params.fetch("body")
+    @comment.commenter_id = params.fetch("commenter_id")
+    @comment.photo_id = params.fetch("photo_id")
+
+    if @comment.valid?
+      @comment.save
+
+      redirect_back(:fallback_location => "/comments", :notice => "Comment created successfully.")
     else
-      render jsonapi_errors: comment
+      render("comment_templates/new_form_with_errors.html.erb")
     end
   end
 
-  def update
-    comment = CommentResource.find(params)
+  def create_row_from_photo
+    @comment = Comment.new
 
-    if comment.update_attributes
-      render jsonapi: comment
+    @comment.body = params.fetch("body")
+    @comment.commenter_id = params.fetch("commenter_id")
+    @comment.photo_id = params.fetch("photo_id")
+
+    if @comment.valid?
+      @comment.save
+
+      redirect_to("/photos/#{@comment.photo_id}", notice: "Comment created successfully.")
     else
-      render jsonapi_errors: comment
+      render("comment_templates/new_form_with_errors.html.erb")
     end
   end
 
-  def destroy
-    comment = CommentResource.find(params)
+  def edit_form
+    @comment = Comment.find(params.fetch("prefill_with_id"))
 
-    if comment.destroy
-      render jsonapi: { meta: {} }, status: 200
+    render("comment_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @comment = Comment.find(params.fetch("id_to_modify"))
+
+    @comment.body = params.fetch("body")
+    
+    @comment.photo_id = params.fetch("photo_id")
+
+    if @comment.valid?
+      @comment.save
+
+      redirect_to("/comments/#{@comment.id}", :notice => "Comment updated successfully.")
     else
-      render jsonapi_errors: comment
+      render("comment_templates/edit_form_with_errors.html.erb")
     end
+  end
+
+  def destroy_row_from_commenter
+    @comment = Comment.find(params.fetch("id_to_remove"))
+
+    @comment.destroy
+
+    redirect_to("/users/#{@comment.commenter_id}", notice: "Comment deleted successfully.")
+  end
+
+  def destroy_row_from_photo
+    @comment = Comment.find(params.fetch("id_to_remove"))
+
+    @comment.destroy
+
+    redirect_to("/photos/#{@comment.photo_id}", notice: "Comment deleted successfully.")
+  end
+
+  def destroy_row
+    @comment = Comment.find(params.fetch("id_to_remove"))
+
+    @comment.destroy
+
+    redirect_to("/comments", :notice => "Comment deleted successfully.")
   end
 end
